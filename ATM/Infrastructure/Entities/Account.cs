08a00894c8cca63c081b8e2e6fbd7ee290d1021a
Okoye emmanuel <chuks.okoye@tenece.com>
2019-04-13 13:27:56 +0100

    initial commit after update to file directory

---
+++ ATM/Infrastructure/Entities/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ATM.Infrastructure.Entities
+{
+    public class Account
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public Customer Customer { get; set; }
+    }
+}
